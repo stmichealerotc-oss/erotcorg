fc7c710fb3e7715b526697e712ae99d5418f7848
Debesay Birhane <debesay304@gmail.com>
2026-02-06 17:33:48 +0800

    Fix seedUsers script to use MONGODB_URI environment variable

--- backend/scripts/seedUsers.js
+++ backend/scripts/seedUsers.js
@@ -7,7 +7,7 @@ const User = require('../models/Users'); // Make sure this path is correct
 
 const seedUsers = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
